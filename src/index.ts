0b7df1d5f3bf562ba7ba457cba235662bbb7af4d
Kirill Novik <kino6052@colorado.edu>
2018-08-08 22:08:45 +0300

    Add main GET methods

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,36 @@
-import axios from 'axios';
 import express from 'express';
 import mongoose from 'mongoose';
-import { dfs } from './utils/tree-of-abstraction-refactor';
+import { Item } from "./schemas/item/item";
+import { Note } from "./schemas/note/note";
+import { ItemNote } from "./schemas/item-note/item-note";
+
 const app = express();
-const Schema = mongoose.Schema;
 
 mongoose.connect('mongodb://test:Test123!@ds011963.mlab.com:11963/iolearn');
 
-
-
 // Application level CRUD
-app.get('/', () => {});
-app.post('/', () => {});
+// app.get('/', () => {});
+// app.post('/', () => {});
 
 // Items CRUD
-app.post('/item', (req, res) => {});
-app.delete('/item/:id', () => {});
-app.get('/item/:id', () => {});
+app.get('/item/:id', (req, res) => {
+    Item.find({ "_id": req.params.id })
+        .then((data) => { res.send(data) })
+        .catch((err) => { res.statusCode = 400; res.send(err); });
+});
 
 // Notes CRUD
-app.post('/note', (req, res) => {});
-app.delete('/note/:id', (req, res) => {
-    res.send("DELETE");
+app.get('/note/:id', (req, res) => {
+    Note.find({ "_id": req.params.id })
+        .then((data) => { res.send(data) })
+        .catch((err) => { res.statusCode = 400; res.send(err); });
 });
-app.get('/note/:id', () => {});
 
 // Items to Notes Relationships
-app.post('/item-note', () => {});
-app.put('/item-note', () => {});
-app.get('/item-note', () => {});
+app.get('/item-note/:id', (req, res) => {
+    ItemNote.find({ "_id": req.params.id })
+        .then((data) => { res.send(data) })
+        .catch((err) => { res.statusCode = 400; res.send(err); });
+});
 
 app.listen('4000', () => console.log("Listening..."));
