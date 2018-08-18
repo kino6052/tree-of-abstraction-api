b3c958d511d3935d55b1dbb48029d28188ee395b
Kirill Novik <jimmynovik@gmail.com>
2018-08-18 20:38:05 +0000

    Add query by title support

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,29 @@ app.get('/item/:id', (req, res) => {
         .catch((err) => { res.statusCode = 400; res.send(err); });
 });
 
+interface ItemQuery {
+    title?: {
+        $regex?: String
+    }
+}
+
+app.get('/item', (req, res) => {
+    let itemQuery: ItemQuery = req.query;
+    let query = <ItemQuery>{};
+    if (itemQuery.title) {
+        query.title = { $regex: `.*${itemQuery.title}.*` }
+    }
+    Item.find(query)
+        .then((data) => { res.send(data) })
+        .catch((err) => { res.statusCode = 400; res.send(err); });
+});
+
+app.post('/item', (req, res) => {
+    Item.find(query)
+        .then((data) => { res.send(data) })
+        .catch((err) => { res.statusCode = 400; res.send(err); });
+});
+
 // Notes CRUD
 app.get('/note/:id', (req, res) => {
     Note.find({ "_id": req.params.id })
@@ -33,4 +56,4 @@ app.get('/item-note/:id', (req, res) => {
         .catch((err) => { res.statusCode = 400; res.send(err); });
 });
 
-app.listen('4000', () => console.log("Listening..."));
+app.listen(process.env.PORT || '8080', () => console.log(`Listening... ${process.env.PORT}`));
