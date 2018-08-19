fd33ae7e85a6a5f93344ce1868b2a5837975e142
kino6052 <kino6052@colorado.edu>
2018-08-19 17:48:26 +0300

    Refactor Structure

--- src/endpoints/item-note.ts
+++ src/endpoints/item-note.ts
@@ -1,4 +1,4 @@
-import { app } from '..';
+import express from 'express';
 import { ItemNote } from '../schemas/item-note/item-note';
 
 interface ItemNoteQuery {
@@ -6,24 +6,24 @@ interface ItemNoteQuery {
   noteId?: String
 }
 
-// Items to Notes Relationships
-app.get('/item-note', (req, res) => {
-  let itemNoteQuery: ItemNoteQuery = req.query
-  ItemNote.find(itemNoteQuery)
-    .then((data) => { res.send(data) })
-    .catch((err) => { res.statusCode = 400; res.send(err); });
-});
-
-app.get('/item-note/:id', (req, res) => {
-    ItemNote.find({ "_id": req.params.id })
+export default (app: ReturnType<typeof express>) => {
+  // Items to Notes Relationships
+  app.get('/item-note', (req, res) => {
+    let itemNoteQuery: ItemNoteQuery = req.query
+    ItemNote.find(itemNoteQuery)
       .then((data) => { res.send(data) })
       .catch((err) => { res.statusCode = 400; res.send(err); });
-});
+  });
 
-app.delete('/item-note/:id', (req, res) => {
-  ItemNote.deleteOne({ "_id": req.params.id })
-    .then((data) => { res.send(data) })
-    .catch((err) => { res.statusCode = 400; res.send(err); });
-});
+  app.get('/item-note/:id', (req, res) => {
+      ItemNote.find({ "_id": req.params.id })
+        .then((data) => { res.send(data) })
+        .catch((err) => { res.statusCode = 400; res.send(err); });
+  });
 
-app.listen(process.env.PORT || '8080', () => console.log(`Listening... ${process.env.PORT}`));
+  app.delete('/item-note/:id', (req, res) => {
+    ItemNote.deleteOne({ "_id": req.params.id })
+      .then((data) => { res.send(data) })
+      .catch((err) => { res.statusCode = 400; res.send(err); });
+  });
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
+import itemEndpoints from './endpoints/item';
+import noteEndpoints from './endpoints/note';
+import itemNoteEndpoints from './endpoints/item-note';
 
 export const jsonBodyParser = bodyParser.json();
 export const app = express();
+
+itemEndpoints(app);
+noteEndpoints(app);
+itemNoteEndpoints(app);
+
 mongoose.connect('mongodb://test:Test123!@ds011963.mlab.com:11963/iolearn');
+app.listen(process.env.PORT || '8080', () => console.log(`Listening... ${process.env.PORT}`));
