4037691ca9bb098deb3b56288b1f5fd5eef0659d
Kirill Novik <jimmynovik@gmail.com>
2018-10-20 09:59:35 +0000

    Add endpoints to the index file

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,9 @@ import bodyParser from 'body-parser';
 import itemEndpoints from './endpoints/item';
 import noteEndpoints from './endpoints/note';
 import itemNoteEndpoints from './endpoints/item-note';
+import itemChildEndpoints from './endpoints/item-child';
+
+const MLAB = 'mongodb://test:Test123!@ds011963.mlab.com:11963/iolearn';
 
 export const jsonBodyParser = bodyParser.json();
 export const app = express();
@@ -11,6 +14,7 @@ export const app = express();
 itemEndpoints(app);
 noteEndpoints(app);
 itemNoteEndpoints(app);
+itemChildEndpoints(app);
 
-mongoose.connect('mongodb://test:Test123!@ds011963.mlab.com:11963/iolearn');
+mongoose.connect(MLAB);
 app.listen(process.env.PORT || '8080', () => console.log(`Listening... ${process.env.PORT}`));
