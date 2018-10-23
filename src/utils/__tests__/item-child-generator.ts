9e9918a789c044ea7b175998f08ae772d710a5a6
kino6052 <kino6052@colorado.edu>
2018-10-23 10:15:21 +0300

    Add Item-Child Generator Skeleton

---
+++ src/utils/__tests__/item-child-generator.ts
@@ -0,0 +1,5 @@
+// 1. Get all Items Array
+// 2. Iterate over the Array
+// 3. Iterate over Each Child in Items
+// 4. Check for presence of Child in item-child Store
+// 5. Conditionally Generate item-child
