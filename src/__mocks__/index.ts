b02cddc7f3b72e1e3090a77a75f837d2e7b07ff6
kino6052 <kino6052@colorado.edu>
2018-10-23 11:06:32 +0300

    Add Promises to Mocks

--- src/__mocks__/index.ts
+++ src/__mocks__/index.ts
@@ -1,1 +1,41 @@
-export const app = { test: 'test' }
+export let app = {
+  get: jest.fn().mockImplementation(
+    (URL) => {
+      switch (URL) {
+        case '/item':
+          return new Promise<Object>(
+            (resolve) => {
+              resolve(
+                {
+                    statusCode: 200,
+                    data: [
+                      {
+                        _id: 1,
+                        title: 'Parent',
+                        children: [2, 3]
+                      },
+                      {
+                        _id: 2,
+                        title: 'Child001',
+                        children: [1]
+                      },
+                      {
+                        _id: 3,
+                        title: 'Child002',
+                        children: [1]
+                      }
+                    ],
+                }
+              )
+            }
+          )
+        default:
+          return new Promise<Object>(
+            (resolve) => {
+              resolve([]);
+            }
+          )
+      }
+    }
+  )
+}
--- src/utils/__tests__/item-child-generator.ts
+++ src/utils/__tests__/item-child-generator.ts
@@ -3,3 +3,15 @@
 // 3. Iterate over Each Child in Items
 // 4. Check for presence of Child in item-child Store
 // 5. Conditionally Generate item-child
+
+import { app } from '../../index';
+
+interface ItemResponse {
+  statusCode: Number,
+  data: Array<Object>
+}
+
+it('should return 200 response for item get REST method', async () => {
+  let itemResponse: ItemResponse = await app.get('/item');
+  expect(itemResponse.statusCode).toEqual(200);
+});
